26e0c89e315e738685c1bc75fa50027d67f45017
Goodwin777 <al.gavrylov@gmail.com>
2015-04-30 15:02:13 +0300

    change way to show credentials of user in class UserData
    Signed-off-by: Goodwin777 <al.gavrylov@gmail.com>

--- input_user.py
+++ input_user.py
@@ -4,22 +4,26 @@ import getpass
 class UsersData:
    'this class get users credentials'
    usersCount = 0
-   cred=[]
+   cred = []
 
    def __init__(self):
       UsersData.usersCount += 1
            
    def credentials(self):
-       self.host=raw_input("please,enter Database host and press \"Return/Enter\": ")
+       self.host = raw_input("please,enter db host and press \"Return/Enter\": ")
        host = self.host
-       print host
-       self.dbname=raw_input("please,enter Database name and press \"Return/Enter\": ")
-       dbname = self.dbname
-       print dbname
-#        self.dbpassword=raw_input("please,enter Database password and press \"Return/Enter\": ")
+       UsersData.cred.append(host)
+        
+       self.dbname = raw_input("please,enter db name and press \"Return/Enter\": ")
+       name = self.dbname
+       UsersData.cred.append(name)
+
        self.dbpassword=getpass.getpass("please,enter Database password and press \"Return/Enter\": ")
-       dbpassword = self.dbpassword
-       print dbpassword
+       password = self.dbpassword
+       UsersData.cred.append(password)
+       cred=UsersData.cred
+       for data in cred:
+           print data
 newUser=UsersData()
 newUser.credentials() 
 
