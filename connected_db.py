bcc3268825ea740b4deb6edeb417b9def9806dea
Goodwin777 <al.gavrylov@gmail.com>
2015-05-02 01:07:30 +0300

    Signed-off-by: Goodwin777 <al.gavrylov@gmail.com>

--- connected_db.py
+++ connected_db.py
@@ -7,23 +7,21 @@ Created on 29 ���. 2015
 @author: Oleksii
 '''
 
- 
+
 import MySQLdb
 
-db = MySQLdb.connect(host="localhost", # your host, usually localhost
-                     user="newuser", # your username
-                      passwd="1234", # your password
-                      db="test") # name of the data base
+db = MySQLdb.connect(host="localhost",  # your host, usually localhost
+                     user="newuser",  # your username
+                     passwd="1234",  # your password
+                     db="test")  # name of the data base
 
 # create a Cursor object. It will let
 # Cursor executes all the queries you want
-cur = db.cursor() 
-
-# Use all the SQL 
+cur = db.cursor()
+#
+# # Use all the SQL
 cur.execute("SELECT * FROM customers")
-
-# print all the first cell of all the rows
-for row in cur.fetchall() :
-    print row[0] 
-    
-    
+#
+# # print all the first cell of all the rows
+for row in cur.fetchall():
+    print row
--- input_user.py
+++ input_user.py
@@ -1,31 +1,42 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import getpass
+import MySQLdb
+
+
 class UsersData:
-   'this class get users credentials'
-   usersCount = 0
-   cred = []
+    'this class get users credentials and connect to defined DB'
+    usersCount = 0
 
-   def __init__(self):
-      UsersData.usersCount += 1
-           
-   def credentials(self):
-       self.host = raw_input("please,enter db host and press \"Return/Enter\": ")
-       host = self.host
-       UsersData.cred.append(host)
-        
-       self.dbname = raw_input("please,enter db name and press \"Return/Enter\": ")
-       name = self.dbname
-       UsersData.cred.append(name)
+    def __init__(self):
+        UsersData.usersCount += 1
 
-       self.dbpassword=getpass.getpass("please,enter Database password and press \"Return/Enter\": ")
-       password = self.dbpassword
-       UsersData.cred.append(password)
-       cred=UsersData.cred
-       for data in cred:
-           print data
-           
-newUser=UsersData()
-newUser.credentials() 
+        self.host = raw_input(
+            "please, enter db host and press \"Return/Enter\": ")
+        self.dbuser = raw_input(
+            "please, enter login and press \"Return/Enter\": ")
 
-     
+        self.dbpassword = getpass.getpass(
+            "please,enter Database password and press \"Return/Enter\": ")
+        self.dbname = raw_input(
+            "please, enter db name and press \"Return/Enter\": ")
+
+        host = self.host
+        user = self.dbuser
+        password = self.dbpassword
+        name = self.dbname
+
+        print host, user, name, password
+        db = MySQLdb.connect(host, user, password, name)
+        cur = db.cursor()
+        #
+        # # Use all the SQL
+        cur.execute("SELECT * FROM customers")
+        #
+        # # print all the first cell of all the rows
+        for row in cur.fetchall():
+            print row
+
+
+newUser = UsersData()
+newUser.dbconnect()
