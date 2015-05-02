50bf29934b40a74ccaff89dc14360679dc186a55
Goodwin777 <al.gavrylov@gmail.com>
2015-05-02 11:29:46 +0300

    added try into input_user.py
    Signed-off-by: Goodwin777 <al.gavrylov@gmail.com>

--- input_user.py
+++ input_user.py
@@ -9,29 +9,35 @@ class UsersData:
     usersCount = 0
 
     def __init__(self):
-        UsersData.usersCount += 1
-        dbhost = raw_input(
-            "please, enter db host and press \"Return/Enter\": ")
-        dbuser = raw_input(
-            "please, enter db user and press \"Return/Enter\": ")
-#         dbpassword = getpass.getpass(
-#             "please,enter password and press \"Return/Enter\": ")#doesn't work right at eclipse
+        try:
+            #             print 'something wrong'
+            #         except:
+            #             _mysql_exceptions.OperationalError
+            UsersData.usersCount += 1
+            dbhost = raw_input(
+                "please, enter db host and press \"Return/Enter\": ")
+            dbuser = raw_input(
+                "please, enter db user and press \"Return/Enter\": ")
+            dbpassword = getpass.getpass(
+                "please,enter password and press \"Return/Enter\": ")  # doesn't work right at eclipse
 
-        dbpassword = '1234'  # use real password when work at Eclipse
-        # should be replaced before publishing
-        dbname = self.dbname = raw_input(
-            "please, enter db name and press \"Return/Enter\": ")
+#             dbpassword = '1234'     # use real password when work at Eclipse
+#                                     # should be commented before publishing
+            dbname = self.dbname = raw_input(
+                "please, enter db name and press \"Return/Enter\": ")
 
-        db = MySQLdb.connect(
-            host=dbhost, user=dbuser, passwd=dbpassword, db=dbname)
-        cur = db.cursor()
-        #
-        # # Use all the SQL
-        cur.execute("SELECT * FROM customers")
-        #
-        # # print all the first cell of all the rows
-        for row in cur.fetchall():
-            print row
+            db = MySQLdb.connect(
+                host=dbhost, user=dbuser, passwd=dbpassword, db=dbname)
+            cur = db.cursor()
+            #
+            # # Use all the SQL
+            cur.execute("SELECT * FROM customers")
+            #
+            # # print all the first cell of all the rows
+
+            print 'Connection with DataBase is stable!'
+        except:
+            print 'Try to run script one more time with correct credential!'
 
 
 newUser = UsersData()
