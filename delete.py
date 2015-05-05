fdab23876f70fc2c84ae44ffa9791699c2f49744
Goodwin777 <al.gavrylov@gmail.com>
2015-05-05 18:45:51 +0300

    added delete.py
    Signed-off-by: Goodwin777 <al.gavrylov@gmail.com>

---
+++ delete.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+'''
+script executes SQL query INSERT_INTO in table "customers' 
+'''
+import MySQLdb
+
+# Open database connection
+db = MySQLdb.connect("localhost", "newuser", "1234", "test")
+
+# prepare a cursor object using cursor() method
+cursor = db.cursor()
+
+# Prepare SQL query to INSERT a record into the database.
+sql = "INSERT INTO customers (row_number, Customer, Customer_ID, Date,Comments) VALUES (1, 'Customer1', 'CustomerID1','20140312','Comment1');"
+try:
+    # Execute the SQL command
+    cursor.execute(sql)
+    # Commit your changes in the database
+    db.commit()
+except:
+    # Rollback in case there is any error
+    db.rollback()
+
+# disconnect from server
+db.close()
